9198a63736cdd51b400f22ee0142ba5aafc947bf
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2020-07-19 16:28:15 +0300

    heyyy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomePage from './homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <HomePage/>
+    <h1> yaniv </h1>
+
+ 
+    </div>
+  );
+}
+
+export default App;
