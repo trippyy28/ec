9e1564b081e6e3a49ffdc87553d024e63703ce7e
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2020-08-24 17:52:12 +0300

    adding button and sign in components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
  
 
-const HatsPage = () =>(
+/*const HatsPage = () =>(
 <div>
 <h1> Hats Page </h1>
 </div>
-	)
+	)*/
 function App() {
   return (
     <div>
@@ -18,6 +19,7 @@ function App() {
      <Switch>
        <Route exact path='/' component={HomePage} />
        <Route exact path ='/shop' component={ShopPage} />
+       <Route exact path ='/signin' component={SignInAndSignUp}/>
      </Switch>
  
     </div>
