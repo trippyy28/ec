32d07b1351b39cd47c8ec944ac7914a5c6d59392
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2020-09-08 11:31:59 +0300

    Updates

---
+++ precache-manifest.5434242aa51392d8f70fb27dd5018d84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287960c0aecde14f32bb21d7fd55764a",
+    "url": "/ec/index.html"
+  },
+  {
+    "revision": "044d553725aa5c2c6f1d",
+    "url": "/ec/static/css/main.bc31d06a.chunk.css"
+  },
+  {
+    "revision": "09c0037234ad5ff21f1a",
+    "url": "/ec/static/js/2.cc91060c.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ec/static/js/2.cc91060c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044d553725aa5c2c6f1d",
+    "url": "/ec/static/js/main.5dd53eb1.chunk.js"
+  },
+  {
+    "revision": "3fc30cce7de760452c26",
+    "url": "/ec/static/js/runtime-main.e9b7e2b5.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ec/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ec/static/media/shopping-bag.30f07071.svg"
+  }
+]);
