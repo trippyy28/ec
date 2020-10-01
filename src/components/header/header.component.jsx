844b2af5653d42092cfd5b59e8b24023f5abb1d1
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2020-10-01 18:05:31 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,47 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropDown from '../cart-dropdown/cart-dropdown.component';
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import './header.styles.scss';
+import "./header.styles.scss";
 
-const Header = ({ currentUser,hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/contact">
         CONTACT
       </Link>
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
+        <div className="option" onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
       ) : (
-        <Link className='option' to='/signin'>
+        <Link className="option" to="/signin">
           SIGN IN
         </Link>
       )}
-      <CartIcon/>
+      <CartIcon />
     </div>
-    {
-      hidden ? null:
-    <CartDropDown/>
-  }
+    {hidden ? null : <CartDropDown />}
   </div>
 );
-const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
-  hidden
-
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,23 +1,20 @@
-
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  
-    apiKey: "AIzaSyDGo73aOq3h0ZYJJA_MY3JRJcEVhHF_DL4",
-    authDomain: "crwn-db-cf8fd.firebaseapp.com",
-    databaseURL: "https://crwn-db-cf8fd.firebaseio.com",
-    projectId: "crwn-db-cf8fd",
-    storageBucket: "crwn-db-cf8fd.appspot.com",
-    messagingSenderId: "2309287960",
-    appId: "1:2309287960:web:2edc4acc26cbc9bac8536d",
-    measurementId: "G-ZGH9FCCC6D"
-  };
-
+  apiKey: "AIzaSyDGo73aOq3h0ZYJJA_MY3JRJcEVhHF_DL4",
+  authDomain: "crwn-db-cf8fd.firebaseapp.com",
+  databaseURL: "https://crwn-db-cf8fd.firebaseio.com",
+  projectId: "crwn-db-cf8fd",
+  storageBucket: "crwn-db-cf8fd.appspot.com",
+  messagingSenderId: "2309287960",
+  appId: "1:2309287960:web:2edc4acc26cbc9bac8536d",
+  measurementId: "G-ZGH9FCCC6D",
+};
 
 firebase.initializeApp(config);
-
+//function that allow us to take the auth object that we got back from our authentication library and then store inside of our db.
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -33,10 +30,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -45,9 +42,9 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
+//google sign in auth
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
